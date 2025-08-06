9805e0eb43eeb4c0927d6da0c71c3c587e1331b9
Samrudh B N <samrudhbn12@gmail.com>
2025-08-06 21:30:42 +0530

    Python list methods
    A file containing python list methods

---
+++ 2_list_methods.py
@@ -0,0 +1,16 @@
+list = ["arun", "akash",21,413.25,True,None,]
+print(list)
+list.append("Sam")
+print(list)
+
+l1 = [34,56,1,78,22,43,89,99,0,12]
+l1.sort()
+print(l1)
+# l1.reverse()
+# print(l1)
+l1.insert(3,4567)
+print(l1)
+l1.pop(2)
+print(l1)
+l1.remove(99)
+print(l1)
